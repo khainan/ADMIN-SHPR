2aba7168b9571d6a66d16cf44556ca976f79aa1c
khainan <muhammadrachim.k@gmail.com>
2022-01-09 13:31:25 +0700

    chore: enhance sidebar styling and html structure

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import classNames from 'classnames';
 
 // styles
 import './styles.scss';
@@ -7,7 +8,11 @@ export default function Dashboard() {
   const [users, setListUsers] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
 
-  const menus = [{ icon: '', title: 'Dashboard' }];
+  const menus = [
+    { title: 'Home' },
+    { title: 'Driver Management' },
+    { title: 'Pick Up' },
+  ];
 
   const nextPage = () => {
     setCurrentPage((page) => page + 1);
@@ -22,34 +27,44 @@ export default function Dashboard() {
   return (
     <div id="dashboard" className="dashboard">
       <div className="dashboard-header">
-        <img alt='' src="https://assets-cdn.shipper.id/shipper-id-v5-web/release-5.5.0/assets/images/logo-shipper.svg" />
+        <img
+          alt=""
+          src="https://assets-cdn.shipper.id/shipper-id-v5-web/release-5.5.0/assets/images/logo-shipper.svg"
+        />
         <div className="dashboard-header-user">
-          <p>{`Hello, ${'Shipper User'}`}</p>
-          <img alt='' />
+          <p>
+            Hello, <span>{'Shipper User'}</span>
+          </p>
+          <div />
         </div>
       </div>
       <div className="dashboard-sidebar">
         {menus.map((menu, index) => (
-          <div className="dashboard-menu" key={index}>
-            <img src={menu.icon} alt={menu.icon} />
+          <div
+            className={classNames('dashboard-sidebar-menu', {
+              active: menu.title === 'Driver Management',
+            })}
+            key={index}
+          >
+            <div />
             <p>{menu.title}</p>
           </div>
         ))}
       </div>
       <div className="dashboard-content">
-          <div className="dashboard-content-header">
-              <div className="dashboard-content-header-title">
-                <h1>DRIVER MANAGEMENT</h1>
-                <p>Data driver yang bekerja dengan anda.</p>
-              </div>
-              <div className="dashboard-content-header-action">
-                <div className="search-input">
-                  <img src="" alt="" />
-                  <input placeholder='Cari driver'/>
-                </div>
-                <button>TAMBAH DRIVER</button>
-              </div>
+        <div className="dashboard-content-header">
+          <div className="dashboard-content-header-title">
+            <h1>DRIVER MANAGEMENT</h1>
+            <p>Data driver yang bekerja dengan anda.</p>
           </div>
+          <div className="dashboard-content-header-action">
+            <div className="search-input">
+              <div />
+              <input placeholder="Cari driver" />
+            </div>
+            <button>TAMBAH DRIVER</button>
+          </div>
+        </div>
       </div>
     </div>
   );
